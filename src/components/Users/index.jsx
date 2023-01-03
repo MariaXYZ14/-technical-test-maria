0fb2e08aa90bb6ee2e4ed14d4175ef27517e81d2
MariaXYZ14 <mariaxyz14@gmail.com>
2023-01-03 13:27:44 +0100

    part 1 done

---
+++ src/components/Users/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { UsersGrid, User, Name, Email, Image } from './styles'
+
+const Users = ({ Usersdata = [] }) => {
+  console.log(Usersdata)
+  return (
+    <UsersGrid>
+      {Usersdata.map(({ email, name, picture }) => (
+        <User key={email}>
+          <Image src={picture.large} />
+          <Name>{name.first} {name.last}</Name>
+          <Email>{email}</Email>
+        </User>
+      ))}
+    </UsersGrid>
+  )
+}
+
+export default Users
---
+++ src/components/Users/styles.js
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled'
+
+export const UsersGrid = styled.div`
+  margin: auto;
+  margin-top: 5rem;
+  width: 20rem;
+  height: 40rem;
+  background-color: #D8F8F6;
+  display: grid;
+  padding: 1rem;
+  border-radius: 0.2rem;
+`
+export const User = styled.div`
+
+`
+export const Name = styled.p`
+  font-weight: bolder;
+  margin-left: 6rem;
+`
+export const Email = styled.p`
+  margin-left :4.5rem;
+`
+export const Image = styled.img`
+  width: 4rem;
+  height: 4rem;
+  display: inline-block;
+  margin-top: 1rem;
+  border-radius: 0.2rem;
+  float: left;
+`
