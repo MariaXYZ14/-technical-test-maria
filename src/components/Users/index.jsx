c70493ea5798685303035cdab64d0fbdcb2c7fa9
MariaXYZ14 <mariaxyz14@gmail.com>
2023-01-03 15:16:59 +0100

    part 2 done

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
-import { UsersGrid, User, Name, Email, Image } from './styles'
+import React, { useState } from 'react'
+import { UsersGrid, User, Name, Email, Image, Button } from './styles'
 
 const Users = ({ Usersdata = [] }) => {
-  console.log(Usersdata)
+  let InitialArray = []
+
+  const [listOfUsers, setListOfUsers] = useState(InitialArray)
+  const [counter, setCounter] = useState(1)
+
+  const newUser = () => {
+    if (counter > 5) {
+      InitialArray = []
+      setCounter(1)
+    } else {
+      InitialArray = Usersdata.slice(0, counter)
+      setCounter(counter + 1)
+    }
+
+    setListOfUsers(InitialArray)
+  }
+  if (!InitialArray) {
+    return <></>
+  }
   return (
-    <UsersGrid>
-      {Usersdata.map(({ email, name, picture }) => (
-        <User key={email}>
-          <Image src={picture.large} />
-          <Name>{name.first} {name.last}</Name>
-          <Email>{email}</Email>
-        </User>
-      ))}
-    </UsersGrid>
+    <>
+      <UsersGrid>
+        {listOfUsers.map(({ email, name, picture }) => (
+          <User key={email}>
+            <Image src={picture.large} />
+            <Name>{name.first} {name.last}</Name>
+            <Email>{email}</Email>
+          </User>
+        ))}
+      </UsersGrid>
+      <Button onClick={newUser}>ADD</Button>
+    </>
   )
 }
 
--- src/components/Users/styles.js
+++ src/components/Users/styles.js
@@ -4,7 +4,7 @@ export const UsersGrid = styled.div`
   margin: auto;
   margin-top: 5rem;
   width: 20rem;
-  height: 40rem;
+  height: 100%;
   background-color: #D8F8F6;
   display: grid;
   padding: 1rem;
@@ -27,4 +27,17 @@ export const Image = styled.img`
   margin-top: 1rem;
   border-radius: 0.2rem;
   float: left;
+`
+export const Button = styled.button`
+  width: 4rem;
+  height: 2rem;
+  margin-left: 66rem;
+  margin-top: 1rem;
+  background-color:#48C4BD;
+  font-weight: bolder;
+  border-radius: 0.15rem;
+  border:none;
+  &:hover {
+    background-color:#A5F3EF;
+  }
 `
