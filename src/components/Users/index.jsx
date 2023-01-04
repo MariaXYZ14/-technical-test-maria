457a973b741196a1d9f87e81759d220d472b831e
MariaXYZ14 <mariaxyz14@gmail.com>
2023-01-04 11:18:46 +0100

    part 3 done

--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -1,30 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { UsersGrid, User, Name, Email, Image, Button } from './styles'
+import Axios from 'axios'
 
-const Users = ({ Usersdata = [] }) => {
-  let InitialArray = []
-
-  const [listOfUsers, setListOfUsers] = useState(InitialArray)
+const Users = () => {
+  const [listOfUsers, setListOfUsers] = useState([])
   const [counter, setCounter] = useState(1)
 
+  useEffect(() => {
+    Axios.get('https://randomuser.me/api/?results=5').then(response => {
+      setListOfUsers(response.data.results)
+    })
+  }, [])
+
   const newUser = () => {
-    if (counter > 5) {
-      InitialArray = []
+    if (counter > 4) {
       setCounter(1)
     } else {
-      InitialArray = Usersdata.slice(0, counter)
       setCounter(counter + 1)
     }
-
-    setListOfUsers(InitialArray)
   }
-  if (!InitialArray) {
+  if (listOfUsers.length === 0) {
     return <></>
   }
   return (
     <>
       <UsersGrid>
-        {listOfUsers.map(({ email, name, picture }) => (
+        {listOfUsers.filter((user, index) => index < counter).map(({ email, name, picture }) => (
           <User key={email}>
             <Image src={picture.large} />
             <Name>{name.first} {name.last}</Name>
@@ -36,5 +37,4 @@ const Users = ({ Usersdata = [] }) => {
     </>
   )
 }
-
 export default Users
